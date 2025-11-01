95b6084cc8dc1e96c701f19deeddca0272cad4e8
Dillon Johnson <me@nollidnosnhoj.com>
2025-10-31 18:43:50 -1000

    install submodules

--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 
 set -e
 
+echo "Get required submodules..."
+git submodule update --init --recursive
+
 IS_WSL=$(grep -qi microsoft /proc/version && echo "1" || echo "0")
 
 if [ "$IS_WSL" == "1" ]; then
-    STOW_PATHS="fastfetch,mise,zsh"
+    STOW_PATHS="fastfetch,mise,starship,zsh"
 else
-    STOW_PATHS="fastfetch,mise,niri,zsh"
+    STOW_PATHS="fastfetch,mise,niri,starship,zsh"
 fi
 
-chmod +x ./install/*.sh
+chmod +x ./install/**/*.sh
 for f in ./install/*.sh; do 
     echo "Running $f"
     source "$f"
+    echo "$f done"
+    echo ""
 done
